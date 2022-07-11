81dfb266eef15b5cf24a20befa749ba67ee0c458
pierre.oucif <pierre.oucif@gadz.org>
2022-07-11 14:36:25 +0200

    Adding PR histogram API

--- application/rest-api-adapter/src/main/java/catlean/monolithic/backend/rest/api/adapter/PullRequestRestApiAdapter.java
+++ application/rest-api-adapter/src/main/java/catlean/monolithic/backend/rest/api/adapter/PullRequestRestApiAdapter.java
@@ -1,6 +1,7 @@
 package catlean.monolithic.backend.rest.api.adapter;
 
 import fr.catlean.monolithic.backend.frontend.contract.api.PullRequestApi;
+import fr.catlean.monolithic.backend.frontend.contract.api.model.HistogramResponseContract;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PullRequestRestApiAdapter implements PullRequestApi {
 
     @Override
-    public ResponseEntity<Void> getPullRequestHistogram() {
+    public ResponseEntity<HistogramResponseContract> getPullRequestHistogram() {
         return PullRequestApi.super.getPullRequestHistogram();
     }
 }
