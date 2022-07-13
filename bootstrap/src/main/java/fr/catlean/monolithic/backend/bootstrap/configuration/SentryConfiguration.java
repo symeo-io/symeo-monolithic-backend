4406e6235ae868313018bca9e6ce78795ccd3742
pierre.oucif <pierre.oucif@gadz.org>
2022-07-13 18:18:10 +0200

    Testing Sentry

--- bootstrap/src/main/java/fr/catlean/monolithic/backend/bootstrap/configuration/SentryConfiguration.java
+++ bootstrap/src/main/java/fr/catlean/monolithic/backend/bootstrap/configuration/SentryConfiguration.java
@@ -1,5 +1,6 @@
 package fr.catlean.monolithic.backend.bootstrap.configuration;
 
+import fr.catlean.monolithic.backend.domain.exception.CatleanException;
 import io.sentry.Sentry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ public class SentryConfiguration {
 
         public SentryInitiator() {
             Sentry.init("https://f4857496207c4352ac9ceb448406e6a8@o1317381.ingest.sentry.io/6570536");
+            Sentry.captureException(CatleanException.builder().code("T.TEST_SENTRY").message("Testing Sentry").build());
         }
     }
 
