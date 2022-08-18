0fb79cb5e85846c19ff34302f36a8526165c7846
pierre.oucif <pierre.oucif@gadz.org>
2022-08-18 09:23:12 +0200

    Fixing datadog integration build

--- .aws/build_infrastructure.sh
+++ .aws/build_infrastructure.sh
@@ -265,7 +265,6 @@ aws cloudformation deploy \
   --parameter-overrides \
        APIKey=${DATADOG_API_KEY} \
        APPKey=${DATADOG_APP_KEY} \
-       CloudwatchLogsGroup=${CloudwatchLogsGroup} \
   --region ${REGION} \
   --stack-name catlean-datadog-integration \
   --capabilities CAPABILITY_IAM \
