d72dc162a06937fe42c71958ea9369c4ddfba3bb
pierre.oucif <pierre.oucif@gadz.org>
2022-08-26 14:29:38 +0200

    Update openapi contract

--- application/rest-api-adapter/src/main/java/io/symeo/monolithic/backend/application/rest/api/adapter/api/LeadTimeRestApiAdapter.java
+++ application/rest-api-adapter/src/main/java/io/symeo/monolithic/backend/application/rest/api/adapter/api/LeadTimeRestApiAdapter.java
@@ -4,7 +4,9 @@ import com.github.javafaker.Faker;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import io.symeo.monolithic.backend.frontend.contract.api.LeadTimeApi;
-import io.symeo.monolithic.backend.frontend.contract.api.model.*;
+import io.symeo.monolithic.backend.frontend.contract.api.model.LeadTimeResponseContract;
+import io.symeo.monolithic.backend.frontend.contract.api.model.LeadTimeResponseContractLeadTime;
+import io.symeo.monolithic.backend.frontend.contract.api.model.MetricsContract;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,30 +21,22 @@ public class LeadTimeRestApiAdapter implements LeadTimeApi {
 
     private final static Faker faker = new Faker();
 
-    @Override
-    public ResponseEntity<LeadTimeBreakDownResponseContract> getLeadTimeBreakDownMetrics(UUID teamId,
-                                                                                         String startDate,
-                                                                                         String endDate) {
-        final LeadTimeBreakDownResponseContract leadTimeBreakDownResponseContract =
-                new LeadTimeBreakDownResponseContract();
-        final LeadTimeBreakDownResponseContractLeadTimeBreakDown leadTimeBreakDown =
-                new LeadTimeBreakDownResponseContractLeadTimeBreakDown();
-        leadTimeBreakDown.setCodingTime(generateLeadTimeStub());
-        leadTimeBreakDown.setReviewLag(generateLeadTimeStub());
-        leadTimeBreakDown.setReviewTime(generateLeadTimeStub());
-        leadTimeBreakDown.setTimeToDeploy(generateLeadTimeStub());
-        leadTimeBreakDownResponseContract.setLeadTimeBreakDown(leadTimeBreakDown);
-        return ResponseEntity.ok(leadTimeBreakDownResponseContract);
-    }
-
     private static LeadTimeResponseContractLeadTime generateLeadTimeStub() {
         final LeadTimeResponseContractLeadTime leadTimeResponseContractLeadTime =
                 new LeadTimeResponseContractLeadTime();
+        leadTimeResponseContractLeadTime.setAverage(getMetricsContractStub());
+        leadTimeResponseContractLeadTime.setCodingTime(getMetricsContractStub());
+        leadTimeResponseContractLeadTime.setReviewLag(getMetricsContractStub());
+        leadTimeResponseContractLeadTime.setReviewTime(getMetricsContractStub());
+        leadTimeResponseContractLeadTime.setTimeToDeploy(getMetricsContractStub());
+        return leadTimeResponseContractLeadTime;
+    }
+
+    private static MetricsContract getMetricsContractStub() {
         final MetricsContract average = new MetricsContract();
         average.setValue(new BigDecimal(Float.toString(faker.number().numberBetween(1, 200) / 10f)));
         average.setTendencyPercentage(new BigDecimal(Float.toString(faker.number().numberBetween(-2000, 2000) / 10f)));
-        leadTimeResponseContractLeadTime.setAverage(average);
-        return leadTimeResponseContractLeadTime;
+        return average;
     }
 
     @Override
