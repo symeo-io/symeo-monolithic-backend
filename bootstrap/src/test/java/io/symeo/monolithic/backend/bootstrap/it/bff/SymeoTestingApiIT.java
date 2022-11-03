d1c5d21aa765b9776d818477284fd321e33eed69
Georges Biaux <georges.biaux@symeo.io>
2022-11-03 07:51:26 +0100

    Add it test

--- bootstrap/src/test/java/io/symeo/monolithic/backend/bootstrap/it/bff/SymeoTestingApiIT.java
+++ bootstrap/src/test/java/io/symeo/monolithic/backend/bootstrap/it/bff/SymeoTestingApiIT.java
@@ -254,4 +254,159 @@ public class SymeoTestingApiIT extends AbstractSymeoBackForFrontendApiIT {
                 .jsonPath("$.testing.test_types.end_to_end").isEqualTo(null)
                 .jsonPath("$.testing.test_types.end_to_end_tendency_percentage").isEqualTo(null);
     }
+
+    @Order(3)
+    @Test
+    void should_compute_testing_metrics_with_history_data() throws SymeoException {
+        // Given
+        final OrganizationEntity organizationEntity = OrganizationEntity.builder()
+                .id(organizationId)
+                .name(organizationName)
+                .build();
+        organizationRepository.save(organizationEntity);
+
+        final String email = faker.gameOfThrones().character();
+        UserMapper.entityToDomain(userRepository.save(
+                UserEntity.builder()
+                        .id(activeUserId)
+                        .onboardingEntity(OnboardingEntity.builder().id(UUID.randomUUID()).hasConfiguredTeam(true).hasConnectedToVcs(true).build())
+                        .organizationEntities(List.of(organizationEntity))
+                        .status(User.ACTIVE)
+                        .email(email)
+                        .build()
+        ));
+        authenticationContextProvider.authorizeUserForMail(email);
+
+        final OrganizationSettingsEntity organizationSettingsEntity = OrganizationSettingsEntity.builder()
+                .id(organizationSettingsId)
+                .organizationId(organizationId)
+                .pullRequestMergedOnBranchRegex("^main$")
+                .build();
+        organizationSettingsRepository.save(organizationSettingsEntity);
+
+        final RepositoryEntity repositoryEntity = repositoryRepository.save(
+                RepositoryEntity.builder()
+                        .defaultBranch(faker.ancient().hero())
+                        .name(repositoryName)
+                        .organizationId(organizationId)
+                        .id(repositoryId)
+                        .build()
+        );
+        teamRepository.save(
+                TeamEntity.builder()
+                        .organizationId(organizationId)
+                        .id(teamId)
+                        .name(faker.gameOfThrones().dragon())
+                        .repositoryIds(List.of(repositoryEntity.getId()))
+                        .build()
+        );
+
+        commitTestingDataRepository.save(
+                CommitTestingDataEntity.builder()
+                        .id(UUID.randomUUID())
+                        .commitSha(faker.gameOfThrones().dragon())
+                        .organizationId(organizationId)
+                        .repositoryName(repositoryEntity.getName())
+                        .branchName(repositoryEntity.getDefaultBranch())
+                        .unitTestCount(100)
+                        .integrationTestCount(20)
+                        .testLineCount(1000)
+                        .codeLineCount(3000)
+                        .coveredBranches(100)
+                        .totalBranchCount(200)
+                        .date(ZonedDateTime.of(
+                                2021,
+                                12,
+                                15,
+                                0,
+                                0,
+                                0,
+                                0,
+                                ZoneId.systemDefault()))
+                        .build()
+        );
+
+        commitTestingDataRepository.save(
+                CommitTestingDataEntity.builder()
+                        .id(UUID.randomUUID())
+                        .commitSha(faker.gameOfThrones().dragon())
+                        .organizationId(organizationId)
+                        .repositoryName(repositoryEntity.getName())
+                        .branchName(repositoryEntity.getDefaultBranch())
+                        .unitTestCount(100)
+                        .integrationTestCount(20)
+                        .testLineCount(1000)
+                        .codeLineCount(3000)
+                        .coveredBranches(100)
+                        .totalBranchCount(200)
+                        .date(ZonedDateTime.of(
+                                2022,
+                                1,
+                                7,
+                                0,
+                                0,
+                                0,
+                                0,
+                                ZoneId.systemDefault()))
+                        .build()
+        );
+
+        commitTestingDataRepository.save(
+                CommitTestingDataEntity.builder()
+                        .id(UUID.randomUUID())
+                        .commitSha(faker.gameOfThrones().dragon())
+                        .organizationId(organizationId)
+                        .repositoryName(repositoryEntity.getName())
+                        .branchName(repositoryEntity.getDefaultBranch())
+                        .unitTestCount(150)
+                        .integrationTestCount(30)
+                        .testLineCount(2000)
+                        .codeLineCount(6000)
+                        .coveredBranches(150)
+                        .totalBranchCount(200)
+                        .date(ZonedDateTime.of(
+                                2022,
+                                1,
+                                15,
+                                0,
+                                0,
+                                0,
+                                0,
+                                ZoneId.systemDefault()))
+                        .build()
+        );
+
+        // Given
+        final String startDate = "2022-01-01";
+        final String endDate = "2022-02-01";
+
+        // When
+        client.get()
+                .uri(getApiURI(TEAMS_REST_API_TESTING, Map.of("team_id", teamId.toString(),
+                        "start_date", startDate, "end_date", endDate)))
+                .exchange()
+                // Then
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.errors").isEmpty()
+                .jsonPath("$.testing.current_start_date").isEqualTo(startDate)
+                .jsonPath("$.testing.current_end_date").isEqualTo(endDate)
+                .jsonPath("$.testing.previous_start_date").isEqualTo("2021-12-01")
+                .jsonPath("$.testing.previous_end_date").isEqualTo(startDate)
+                .jsonPath("$.testing.coverage.value").isEqualTo(75f)
+                .jsonPath("$.testing.coverage.tendency_percentage").isEqualTo(50f)
+                .jsonPath("$.testing.test_count.value").isEqualTo(180)
+                .jsonPath("$.testing.test_count.tendency_percentage").isEqualTo(50f)
+                .jsonPath("$.testing.test_to_code_ratio.value").isEqualTo(0.25f)
+                .jsonPath("$.testing.test_to_code_ratio.tendency_percentage").isEqualTo(0f)
+                .jsonPath("$.testing.test_to_code_ratio.code_line_count").isEqualTo(6000)
+                .jsonPath("$.testing.test_to_code_ratio.test_line_count").isEqualTo(2000)
+                .jsonPath("$.testing.test_types.unit").isEqualTo(150)
+                .jsonPath("$.testing.test_types.unit_tendency_percentage").isEqualTo(50f)
+                .jsonPath("$.testing.test_types.integration").isEqualTo(30)
+                .jsonPath("$.testing.test_types.integration_tendency_percentage").isEqualTo(50f)
+                .jsonPath("$.testing.test_types.end_to_end").isEqualTo(null)
+                .jsonPath("$.testing.test_types.end_to_end_tendency_percentage").isEqualTo(null);
+    }
 }
